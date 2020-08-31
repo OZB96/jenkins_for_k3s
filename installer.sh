9b8cc93858e010b1b4042d288139c06cabe4513b
Ubuntu <ubuntu@ip-10-0-20-202.me-south-1.compute.internal>
2020-08-31 20:11:38 +0000

    fix

--- installer.sh
+++ installer.sh
@@ -5,7 +5,9 @@ echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt
 sudo apt-get update -y
 sudo apt-get install helm -y
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
-k3s kubectl create serviceaccount --namespace jenkins tiller
-k3s kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=jenkins:tiller
-k3s kubectl patch deploy --namespace jenkins tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
-helm install my-release bitnami/jenkins
+sudo k3s kubectl create serviceaccount --namespace jenkins tiller
+sudo k3s kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=jenkins:tiller
+sudo k3s kubectl patch deploy --namespace jenkins tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
+helm repo add bitnami https://charts.bitnami.com/bitnami
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+helm install jenkins bitnami/jenkins
