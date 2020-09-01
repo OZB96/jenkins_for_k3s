1719d8aa3e89a24e6cc5a0bdf67aaae4f47da750
Omar Bazaid <omar.vip3@gmail.com>
2020-09-01 23:08:44 +0300

    add sudo

--- installer.sh
+++ installer.sh
@@ -4,6 +4,7 @@ sudo apt-get install apt-transport-https --yes
 echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
 sudo apt-get update -y
 sudo apt-get install helm -y
+sudo chmod a+r /etc/rancher/k3s/k3s.yaml
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 sudo k3s kubectl create serviceaccount --namespace jenkins tiller
 sudo k3s kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=jenkins:tiller
@@ -13,5 +14,5 @@ sudo k3s kubectl patch deploy --namespace jenkins tiller-deploy -p '{"spec":{"te
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo add stable https://kubernetes-charts.storage.googleapis.com
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
-helm install jenkins stable/jenkins
+sudo helm install jenkins stable/jenkins
 sudo k3s kubectl apply -f ingress.yaml -f service.yaml
