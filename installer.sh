f6c90643812d3ad3e69bb7784c39b00e537f8dcc
Omar Bazaid <omar.vip3@gmail.com>
2020-09-02 01:26:33 +0300

    fix namespace

--- installer.sh
+++ installer.sh
@@ -6,10 +6,10 @@ sudo apt-get update -y
 sudo apt-get install helm -y
 sudo chmod a+r /etc/rancher/k3s/k3s.yaml
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
-sudo k3s kubectl create serviceaccount --namespace jenkins tiller
-sudo k3s kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=jenkins:tiller
 sudo k3s kubectl create namespace jenkins
 sudo k3s kubectl config set-context --current --namespace jenkins
+sudo k3s kubectl create serviceaccount --namespace jenkins tiller
+sudo k3s kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=jenkins:tiller
 sudo k3s kubectl patch deploy --namespace jenkins tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo add stable https://kubernetes-charts.storage.googleapis.com
